82db7f3582b53e65e28b2dbace473c5ff9d813b5
Jen Wachter <jenleighkelly@gmail.com>
2014-08-06 10:50:59 -0400

    Moved backup and cleanup into db folder. Getting ready fo files backup scripts.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var backup = require("./lib/backup");
-var cleanup = require("./lib/cleanup");
-
 module.exports = {
-    backup: backup,
-    cleanup: cleanup
+  db: {
+    backup: require("./lib/db/backup"),
+    cleanup: require("./lib/db/cleanup")
+  },
+  files: {
+    backup: require("./lib/files/backup"),
+    cleanup: require("./lib/files/cleanup")
+  }
 };
